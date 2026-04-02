343128ad46c0b3fa2f02689caa975b761fe16da9
Joaovit-costa <joaovit.costa0@gmail.com>
2026-04-02 10:36:22 -0300

    range de ataque definido

--- project-room/script/entidades/inimigos.gd
+++ project-room/script/entidades/inimigos.gd
@@ -11,17 +11,33 @@ class_name inimigo
 @export var multiplicadores := [1.0, 1.3, 1.6]
 
 @export var protagonista_ref: protagonista
+@onready var alcance: Area2D = $Area2D
 
 var cooldown: float = 0.0
+var player_em_range: bool = false
 
 const Mecanicas = preload("res://script/Mecanicas.gd")
 var mecanicas = Mecanicas.new()
 
 func _ready() -> void:
 	randomize()
+	alcance.body_entered.connect(_on_area_body_entered)
+	alcance.body_exited.connect(_on_area_body_exited)
+
+func _on_area_body_entered(body: Node) -> void:
+	if body is protagonista:
+		protagonista_ref = body
+		player_em_range = true
+
+func _on_area_body_exited(body: Node) -> void:
+	if body == protagonista_ref:
+		protagonista_ref = null
+		player_em_range = false
 
 func _physics_process(delta: float) -> void:
-	if protagonista_ref != null and cooldown <= 0.0:
+	
+	if protagonista_ref != null and cooldown <= 0.0 and player_em_range:
 		cooldown = mecanicas.atacar(protagonista_ref, cooldowns, forca, multiplicadores)
+
 	elif cooldown > 0.0:
 		cooldown -= delta
--- project-room/script/entidades/protagonista.gd
+++ project-room/script/entidades/protagonista.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 class_name protagonista
 
 @export var alvo: inimigo
-@onready var alcance := Area2D
+@onready var alcance: Area2D = $Area2D
 
 var vitalidade: int = 100
 var defesa: int = 0
@@ -10,44 +10,50 @@ var forca: int = 10
 var velocidade: int = 0
 var inteligencia: int = 0
 
-var cooldowns := [1.0, 1.3, 1.7]
-var multiplicadores := [1.0, 1.2, 1.5]
+var cooldowns: Array[float] = [1.0, 1.3, 1.7]
+var multiplicadores: Array[float] = [1.0, 1.2, 1.5]
 var cooldown: float = 0.0
+var inimigo_no_range: bool = false
 
-const SPEED = 300.0
+const SPEED: float = 300.0
 
 const Mecanicas = preload("res://script/Mecanicas.gd")
 var mecanicas = Mecanicas.new()
 
 func _ready() -> void:
 	randomize()
-	
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("ok")
+	alcance.body_entered.connect(_on_area_body_entered)
+	alcance.body_exited.connect(_on_area_body_exited)
+
+func _on_area_body_entered(body: Node) -> void:
+	if body is inimigo:
+		alvo = body
+		inimigo_no_range = true
+
+func _on_area_body_exited(body: Node) -> void:
+	if body == alvo:
+		alvo = null
+		inimigo_no_range = false
 
 func _physics_process(delta: float) -> void:
-	var directionx := Input.get_axis("ui_left", "ui_right")
-	if directionx:
-		velocity.x = directionx * SPEED
+	var direction_x: float = Input.get_axis("ui_left", "ui_right")
+	if direction_x != 0.0:
+		velocity.x = direction_x * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0.0, SPEED)
 
-	var directiony := Input.get_axis("ui_up", "ui_down")
-	if directiony:
-		velocity.y = directiony * SPEED
+	var direction_y: float = Input.get_axis("ui_up", "ui_down")
+	if direction_y != 0.0:
+		velocity.y = direction_y * SPEED
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0.0, SPEED)
 
 	if Input.is_action_just_pressed("ui_attack") and cooldown <= 0.0:
-		if alvo != null:
+		if alvo != null and inimigo_no_range:
 			cooldown = mecanicas.atacar(alvo, cooldowns, forca, multiplicadores)
+			print("atacado")
 
-	elif cooldown > 0.0:
+	if cooldown > 0.0:
 		cooldown -= delta
-
-	if alvo != null:
-		pass
-	else:
-		print("Sem alvo definido", " ", cooldown)
 
 	move_and_slide()
