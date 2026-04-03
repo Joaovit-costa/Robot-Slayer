ef9d7c91676eb7ae10b8a91431ce5d9f16502de2
CnrcGM <cristhianoag@gmail.com>
2026-04-02 21:14:10 -0300

    Lista de itens incompleta
    Lista de itens vou continuar em casa

---
+++ project-room/script/menu/lista_itens.gd
@@ -0,0 +1,10 @@
+var itens := [{"nome": "Lume mod",
+			"tipo": "equipavel",
+			"quantidade": 1,
+			"descricao":"chip de fogo",
+			"buffs": {"ataque_percentual": 10,
+			"efeito": "fogo",
+			"dps": 1.5,
+			"duracao": 2},
+			"link": "arma"			
+			}]
