1480806904b6594add698e20e0b9bfc5e074693a
CnrcGM <cristhianoag@gmail.com>
2026-05-08 16:14:44 -0300

    Animações de andar prontas

--- project-room/script/entidades/protagonista.gd
+++ project-room/script/entidades/protagonista.gd
@@ -47,7 +47,7 @@ var inventario_ref: Inventario
 
 
 # ============ MUSICA ============
-var musica_normal = preload("res://res/sons/Musica_Sala.mp3") # <-- confere caminho
+var musica_normal = preload("res://res/sons/Musica_Ambiente_1.mp3") # <-- confere caminho
 var musica_low_hp = preload("res://res/sons/Música_um_coração.mp3") # <-- cuidado com acento
 var em_perigo: bool = false
 # =================================
@@ -115,7 +115,6 @@ func _physics_process(delta: float) -> void:
 
 	# ============ VERIFICAR MOVIMENTO ============
 	var esta_andando = velocity.length() > 10
-	atualizar_animacao(direction_x, direction_y)
 	# =============================================
 
 	# ============ ATAQUE ============
@@ -223,7 +222,6 @@ func _adicionar_drop_pendente(nome: String, raridade: int, quantidade: int) -> v
 		"quantidade": quantidade
 	})
 
-
 func _entregar_drops_pendentes() -> void:
 	if inventario_ref == null:
 		inventario_ref = get_tree().get_first_node_in_group("inventario_principal") as Inventario
@@ -239,45 +237,6 @@ func _entregar_drops_pendentes() -> void:
 		)
 
 	drops_pendentes.clear()
-
-func atualizar_animacao(direction_x: float, direction_y: float) -> void:
-
-	if direction_x == 0 and direction_y == 0:
-
-		match ultima_direcao:
-			"up":
-				anim.play("idle_up")
-
-			"down":
-				anim.play("idle_down")
-
-			"left":
-				anim.play("idle_left")
-
-			"right":
-				anim.play("idle_right")
-
-	else:
-
-		if abs(direction_x) > abs(direction_y):
-
-			if direction_x > 0:
-				anim.play("walk_right")
-				ultima_direcao = "right"
-
-			else:
-				anim.play("walk_left")
-				ultima_direcao = "left"
-
-		else:
-
-			if direction_y > 0:
-				anim.play("walk_down")
-				ultima_direcao = "down"
-
-			else:
-				anim.play("walk_up")
-				ultima_direcao = "up"
-
+	
 func _on_area_2d_body_entered(_body: Node) -> void:
 	pass
