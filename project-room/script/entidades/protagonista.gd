84e9a462814c21b672a5f0e10a67a333afd59b61
CnrcGM <cristhianoag@gmail.com>
2026-06-01 00:14:56 -0300

    Reapply "Merge branch 'main' of https://github.com/Joaovit-costa/Robot-Slayer"
    This reverts commit bab56d47386611b21d4055962287d95159eb8fb4.

--- project-room/script/entidades/protagonista.gd
+++ project-room/script/entidades/protagonista.gd
@@ -120,7 +120,6 @@ func _ready() -> void:
 	
 	call_deferred("_sincronizar_menu_status")
 
-
 func _physics_process(delta: float) -> void:
 	# ============ MOVIMENTO ============
 	var direcao = Vector2(
@@ -366,7 +365,6 @@ func _solicitar_salvamento() -> void:
 
 func recalcular_experiencia_dropada() -> void:
 	experienciaDropada = 0
-
 	for alvo in alvos:
 		if alvo == null or not is_instance_valid(alvo):
 			continue
@@ -378,7 +376,6 @@ func recalcular_experiencia_dropada() -> void:
 
 
 func _acumular_drops_do_inimigo(alvo: inimigo) -> void:
-
 	if alvo == null:
 		return
 
